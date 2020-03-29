68a90af7a7a0db3bf293a2ca0d5ceadd973072eb
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-03-29 16:00:40 -0400

    exception raised when user identifies bug

--- gittracker/util/exceptions.py
+++ gittracker/util/exceptions.py
@@ -1,3 +1,6 @@
+from inspect import currentframe, getouterframes
+
+
 class GitTrackerError(Exception):
     pass
 
@@ -13,3 +16,11 @@ class NoGitdirError(GitTrackerError):
         msg = f"{repo_path} does not appear to be a git repository " \
               "(no .git directory found)"
 
+
+class BugIdentified(GitTrackerError):
+    # handles exception info logging for bugs identified by users via the
+    # `utils.prompt_input` function
+    def __init__(self):
+        input_caller = getouterframes(currentframe())[2].function
+        msg = f"Bug identified via user input from function: {input_caller}"
+        super().__init__(msg)
