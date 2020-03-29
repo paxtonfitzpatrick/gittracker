caa362cda809d25e04b1eddb681e58b8230d83be
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-03-29 16:01:41 -0400

    code for manual addition of paths to known repos

--- gittracker/repofile/repofile.py
+++ gittracker/repofile/repofile.py
@@ -4,7 +4,7 @@ from ..util.exceptions import RepoNotFoundError, NoGitdirError
 from ..util.util import log_error, prompt_input, validate_repo
 
 
-KNOWN_REPOS_FPATH = opj(Path(__file__).parents[2], 'log', 'known-repos')
+KNOWN_REPOS_FPATH = opj(Path(__file__).parents[1], 'log', 'known-repos')
 
 
 def load_known_repos():
@@ -13,18 +13,37 @@ def load_known_repos():
     return paths
 
 
+@log_error
 def manual_add(repo_path):
-    path = realpath(repo_path)
+    full_path = realpath(repo_path)
     try:
-        validate_repo(repo_path)
+        validate_repo(full_path)
+        valid = True
     except RepoNotFoundError as e:
-        prompt =
+        # if directory doesn't exist at given path, ask for confirmation
+        prompt = f"{full_path} does not appear to be a directory. Add it anyway?"
+        valid = prompt_input(prompt, default='no')
+    except NoGitdirError as e:
+        # if directory exists but isn't a git repository, ask for confirmation
+        prompt = f"{full_path} does not appear to be a git repository. Add it anyway?"
+        valid = prompt_input(prompt, default='no')
+
+    if valid:
+        if _is_tracked(full_path):
+            # don't add a duplicate if the repository is already being tracked
+            print(f"{full_path} is already tracked by GitTracker")
+        else:
+            with open(KNOWN_REPOS_FPATH, 'a') as f:
+                f.write(f"{full_path}\n")
 
 
+def _is_tracked(repo_path):
+    tracked = load_known_repos()
+    if repo_path in tracked:
+        return True
+    else:
+        return False
 
-
-
-    with open(KNOWN_REPOS_FPATH, )
 
 
 
