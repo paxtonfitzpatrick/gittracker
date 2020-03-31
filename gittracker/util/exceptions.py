41d7461825e73a00897c87888f21ac6ea9962ef9
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-03-31 17:00:55 -0400

    exception class for empty logfile

--- gittracker/util/exceptions.py
+++ gittracker/util/exceptions.py
@@ -1,20 +1,10 @@
 from inspect import currentframe, getouterframes
 
+from ..repofile.repofile import TRACKED_REPOS_FPATH
+
 
 class GitTrackerError(Exception):
     pass
-
-
-class RepoNotFoundError(GitTrackerError):
-    def __init__(self, repo_path):
-        msg = f"{repo_path} does not appear to be a directory"
-        super().__init__(msg)
-
-
-class NoGitdirError(GitTrackerError):
-    def __init__(self, repo_path):
-        msg = f"{repo_path} does not appear to be a git repository " \
-              "(no .git directory found)"
 
 
 class BugIdentified(GitTrackerError):
@@ -23,4 +13,23 @@ class BugIdentified(GitTrackerError):
     def __init__(self):
         input_caller = getouterframes(currentframe())[2].function
         msg = f"Bug identified via user input from function: {input_caller}"
+        super().__init__(msg)
+
+
+class NoGitdirError(GitTrackerError):
+    def __init__(self, repo_path):
+        msg = f"{repo_path} does not appear to be a git repository " \
+              "(no .git directory found)"
+        super().__init__(msg)
+
+
+class NoTrackedReposError(GitTrackerError):
+    def __init__(self, repofile_path=TRACKED_REPOS_FPATH):
+        msg = f"No repositories tracked in logfile at {repofile_path}"
+        super().__init__(msg)
+
+
+class RepoNotFoundError(GitTrackerError):
+    def __init__(self, repo_path):
+        msg = f"{repo_path} does not appear to be a directory"
         super().__init__(msg)
