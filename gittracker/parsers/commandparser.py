3061afab5ece66577c943eede76483971da8086f
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-04-02 20:03:26 -0400

    (mostly) functional CommandParser class

--- gittracker/parsers/commandparser.py
+++ gittracker/parsers/commandparser.py
@@ -2,29 +2,48 @@ from argparse import ArgumentParser
 
 
 class CommandParser(ArgumentParser):
-    def __init__(self, name=None, py_function=None, aliases=None, subcommands=None, **kwargs):
+    def __init__(
+            self,
+            name=None,
+            aliases=None,
+            py_function=None,
+            subcommands=None,
+            short_description=None,
+            **kwargs
+    ):
         self.name = name
-        if aliases is None:
-            self.aliases = []
-        else:
-            self.aliases = aliases
+        self.aliases = [] if aliases is None else aliases
         self.py_function = py_function
-        if subcommands is not None and not hasattr(subcommands, '__iter__'):
+        if subcommands is None:
+            subcommands = []
+        elif not hasattr(subcommands, '__iter__'):
                 subcommands = [subcommands]
         self.subcommands = subcommands
-
-        self.default_subcommand = None
-        self.options = None
+        self.short_description = short_description
         self.subcommand_parsers = None
+        self._default_subcommand = None
         super().__init__(**kwargs)
-
-        self.add_subcommand_parsers()
-
-    # TODO: setter for self.default_subcommand attr
 
     @property
     def all_names(self):
         return (self.name, *self.aliases)
+
+    @property
+    def default_subcommand(self):
+        return self._default_subcommand
+
+    @default_subcommand.setter
+    def default_subcommand(self, cmd):
+        if cmd not in self.subcommands:
+            raise ValueError("default subcommand must be an available subcommand")
+        self._default_subcommand = cmd
+
+    @default_subcommand.getter
+    def get_default_subcommand(self):
+        if self._default_subcommand is not None:
+            return self._default_subcommand
+        else:
+            raise ValueError("default subcommand not set")
 
     def __eq__(self, other):
         return other in self.all_names
@@ -33,42 +52,11 @@ class CommandParser(ArgumentParser):
         try:
             cmd = raw_args[0]
         except IndexError:
-            cmd = self.default_subcommand
+            try:
+                cmd = self.get_default_subcommand()
+            except ValueError:
+                parsed_args = self.parse_args(raw_args)
+                self.py_function(**vars(parsed_args))
 
-        if cmd in self.subcommands:
-            subcmd
-
-
-
-
-
-
-
-    # def add_subcommand_parsers(self):
-    #     if self.subcommands is None:
-    #         return
-    #
-    #     subcommands = self.add_subparsers(title='subcommands')
-    #     for sc in self.subcommands:
-    #         sc.add_as_subparser(subcommands)
-
-
-    def add_as_subparser(self, superparser_group):
-
-
-
-
-
-    def add_argument(self, arg, aliases=None, **kwargs):
-        if aliases is not
-
-
-
-
-
-
-    def parse_args(self, **kwargs):
-
-
-
-
+        subcmd = self.subcommands[self.subcommands.index(cmd)]
+        subcmd(raw_args[1:])
