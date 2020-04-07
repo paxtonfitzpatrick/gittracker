5cc7f59cc817a9d504f395973e4f18258bc9dda3
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-04-07 16:16:48 -0400

    wrote pre-install and install scripts for travis tests

---
+++ tests/ci-scripts/before_install.sh
@@ -0,0 +1,32 @@
+# deactivate travis-provided virtual environment on linux to set up conda instead
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]
+then
+  deactivate
+fi
+
+# set conda path
+echo "setting conda path"
+export MINICONDA_PATH=$HOME/miniconda
+export MINICONDA_LIB_BIN_PATH=$MINICONDA_PATH/Library/bin
+
+if [[ "$TRAVIS_OS_NAME" == "windows" ]]
+then
+  export MINICONDA_SUB_PATH=$MINICONDA_PATH/Scripts
+  export MINICONDA_PATH_WIN=`cygpath --windows $MINICONDA_PATH`
+else
+  export MINICONDA_SUB_PATH=$MINICONDA_PATH/bin
+fi
+
+# download miniconda installer if not running Windows
+# (Windows will use choco to install miniconda instead)
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]
+then
+  mkdir -p $HOME/download
+  echo "downloading miniconda installer for linux"
+  wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $HOME/download/miniconda.sh
+elif [[ "$TRAVIS_OS_NAME" == "osx" ]]
+then
+  mkdir -p $HOME/download
+  echo "downloading miniconda installer for MacOS"
+  wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O $HOME/download/miniconda.sh
+fi
---
+++ tests/ci-scripts/install.sh
@@ -0,0 +1,28 @@
+# install miniconda
+if [[ "$TRAVIS_OS_NAME" == "windows" ]]
+then
+  echo "installing miniconda for windows"
+  choco install miniconda3 --params="'/JustMe /AddToPath:1 /D:$MINICONDA_PATH_WIN'"
+else
+  echo "installing miniconda for posix"
+  bash $HOME/download/miniconda.sh -b -u -p $MINICONDA_PATH
+fi
+
+# update path
+export PATH="$MINICONDA_PATH:$MINICONDA_SUB_PATH:$MINICONDA_LIB_BIN_PATH:$PATH"
+
+source $MINICONDA_PATH/etc/profile.d/conda.sh
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+# Useful for debugging any issues with conda
+conda info -a
+
+# create and activate conda environment
+echo "running in ${TRAVIS_OS_NAME} environment"
+echo "creating environment with Python ${PYTHON_VERSION}"
+conda create -q -n test-environment --yes python=$PYTHON_VERSION pip pytest
+conda activate test-environment
+# install package with pip to test pip installation process
+pip install --quiet --upgrade pip
+pip install .
