c6e4937553cadcf1f5cfd018098e116393806764
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-04-10 21:51:21 -0400

    helpers file for test functions

---
+++ tests/tests/helpers.py
@@ -0,0 +1,13 @@
+import shlex
+from pathlib import PurePath
+from subprocess import PIPE, run
+
+
+def test_input(cmd):
+    # helper function that formats and tests command line input
+    # split args
+    cmd = shlex.split(cmd)
+    # deal with POSIX/Windows path formatting
+    cmd = [str(PurePath(arg)) if '/' in arg else arg for arg in cmd]
+    result = run(cmd, stderr=PIPE)
+    return result.returncode, result.stderr
