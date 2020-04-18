53d982530b0ff47ecb54d660b5795d56eee42b83
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-04-18 17:43:41 -0400

    custom converters for configparser

--- tests/helpers/helpers.py
+++ tests/helpers/helpers.py
@@ -1,4 +1,5 @@
 import shlex
+from collections import namedtuple
 from pathlib import PurePath
 from subprocess import PIPE, run
 
@@ -11,3 +12,21 @@ def run_command(cmd):
     cmd = [str(PurePath(arg)) if '/' in arg else arg for arg in cmd]
     result = run(cmd, stdout=PIPE, stderr=PIPE, encoding='UTF-8')
     return result.returncode, result.stdout, result.stderr
+
+
+###########################################
+# custom type converters for ConfigParser #
+###########################################
+def _get_multiline_list(val):
+    return val.splitlines()[1:]
+
+
+def _get_diff_list(val):
+    Diff = namedtuple('Diff', ('a_name', 'b_name', 'change_type'))
+    return [Diff(*change.split(', ')) for change in val.splitlines()[1:]]
+
+
+CONVERTERS = {
+    'difflist': _get_diff_list,
+    'list': _get_multiline_list
+}
