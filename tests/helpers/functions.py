4b23b0a51024953760db1b7b73d03e12766cb276
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-04-18 22:21:07 -0400

    added docstrings

--- tests/helpers/functions.py
+++ tests/helpers/functions.py
@@ -1,15 +1,16 @@
 import shlex
 from collections import namedtuple
-from pathlib import PurePath
+from pathlib import Path
+from shutil import copy2
 from subprocess import PIPE, run
 
 
 def run_command(cmd):
-    # helper function that formats and tests command line input
+    """helper function that formats and tests command line input"""
     # split args
     cmd = shlex.split(cmd)
     # deal with POSIX/Windows path formatting
-    cmd = [str(PurePath(arg)) if '/' in arg else arg for arg in cmd]
+    cmd = [str(Path(arg)) if '/' in arg else arg for arg in cmd]
     result = run(cmd, stdout=PIPE, stderr=PIPE, encoding='UTF-8')
     return result.returncode, result.stdout, result.stderr
 
@@ -18,10 +19,23 @@ def run_command(cmd):
 # custom type converters for ConfigParser #
 ###########################################
 def _get_multiline_list(val):
+    """
+    splits a newline-separated list of values for a single option into a
+    list
+    NOTE: first item should be on a different line from the option key
+    """
     return val.splitlines()[1:]
 
 
 def _get_diff_list(val):
+    """
+    specifcally for use in the MockIndex class; splits a
+    newline-separated list of values for a single option (where each
+    value is comma-separated list with length 3) into a list of
+    namedtuple types.
+    NOTE: the resultant field names of the namedtuple correspond to the
+    comma-separated values IN ORDER
+    """
     Diff = namedtuple('Diff', ('a_name', 'b_name', 'change_type'))
     return [Diff(*change.split(', ')) for change in val.splitlines()[1:]]
 
@@ -30,3 +44,4 @@ CONVERTERS = {
     'difflist': _get_diff_list,
     'list': _get_multiline_list
 }
+
