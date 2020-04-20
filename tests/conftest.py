e6c1fe963e1533d32828e8978c6096a4ef99f49e
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-04-20 16:48:24 -0400

    setup and teardown for expected output dir

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,36 +2,43 @@ import pytest
 from pathlib import Path
 from shutil import copy2, rmtree
 from .helpers.mock_repo import MockRepo
-
-REPO_CONFIGS_DIR = Path(__file__).resolve().parent.joinpath('repo-configs')
-MOCK_REPOS_DIR = REPO_CONFIGS_DIR.parent.joinpath('mock-repos')
+# have to import MockRepo from helpers subpackage, and some helper funcs
+# use these paths, so paths must be defined in functions file to avoid
+# circular import
+from .helpers.functions import (
+    MOCK_OUTPUT_DIR,
+    MOCK_REPOS_DIR,
+    REPO_CONFIGS_DIR
+)
 
 
 @pytest.fixture(scope='session')
-def setup_repository(monkeypatch):
+def mock_repo(monkeypatch):
     # ==== SETUP ====
     assert REPO_CONFIGS_DIR.is_dir()
     MOCK_REPOS_DIR.mkdir()
+    MOCK_OUTPUT_DIR.mkdir()
     # monkeypatch `git.Repo` object at session level
     monkeypatch.setattr('gittracker.tracker.tracker.Repo', MockRepo)
 
-    def _generate_from_config(config_file):
+    def _setup_repo(config_file):
         """dynamically creates mock repo directories as needed"""
         config_path = REPO_CONFIGS_DIR.joinpath(config_file)
         assert config_path.is_file()
-        # repo dirname is config file's basename with "repo-" removed
         repo_path = MOCK_REPOS_DIR.joinpath(config_file.stem[5:])
-        # repo structure have already been created for previous test
+        # repo/data file may have already been created for previous test
         if not repo_path.is_dir():
             # create directory, add .git directory and config file
             repo_path.mkdir()
             repo_path.joinpath('.git').mkdir()
             copy2(config_path, repo_path)
-
-        return repo_path
+        # return as one-item list so tests can use outer `get_status`
+        # function instead of `_single_repo_status`
+        return [repo_path]
 
     # yield function to tests as fixture
-    yield _generate_from_config
+    yield _setup_repo
 
     # ==== TEARDOWN ====
     rmtree(MOCK_REPOS_DIR)
+    rmtree(MOCK_OUTPUT_DIR)
