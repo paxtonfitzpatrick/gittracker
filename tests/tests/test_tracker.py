64caba627c3e479ab05db1274c60886dce5786e8
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-04-21 01:38:42 -0400

    moment of truth... let's see if this works

---
+++ tests/tests/test_tracker.py
@@ -0,0 +1,9 @@
+from gittracker.tracker.tracker import get_status
+from ..helpers.functions import matches_expected_output
+
+
+def test_even_clean(mock_repo):
+    # most simple case: repo is even with remote with no local changes
+    repo = mock_repo('clean-even.cfg')
+    output = get_status(repo)
+    assert matches_expected_output('clean-even', output['even-clean'])
