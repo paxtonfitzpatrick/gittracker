73dd15efa91e0f74a5119c7ebfa39cd911744107
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-04-21 02:42:52 -0400

    getting the name of the file right often helps

--- tests/tests/test_tracker.py
+++ tests/tests/test_tracker.py
@@ -4,6 +4,6 @@ from ..helpers.functions import matches_expected_output
 
 def test_even_clean(mock_repo):
     # most simple case: repo is even with remote with no local changes
-    repo = mock_repo('clean-even.cfg')
+    repo = mock_repo('even-clean.cfg')
     output = get_status(repo)
-    assert matches_expected_output('clean-even', output['even-clean'])
+    assert matches_expected_output('even-clean', output['even-clean'])
