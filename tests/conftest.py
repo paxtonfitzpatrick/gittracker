bcc3a76c4ef23a510121214b7c274ac1581c9980
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-04-21 04:15:02 -0400

    another pathlib/str mismatch

--- tests/conftest.py
+++ tests/conftest.py
@@ -37,8 +37,7 @@ def mock_repo():
             repo_path.mkdir()
             repo_path.joinpath('.git').mkdir()
             copy2(config_path, repo_path)
-        # return as one-item list for use in `get_status` function
-        return [repo_path]
+        return repo_path
 
     # yield function to tests as fixture
     yield _setup_repo
--- tests/tests/test_tracker.py
+++ tests/tests/test_tracker.py
@@ -5,5 +5,5 @@ from ..helpers.functions import matches_expected_output
 def test_even_clean(mock_repo):
     # most simple case: repo is even with remote with no local changes
     repo = mock_repo('even-clean.cfg')
-    output = get_status(repo)
-    assert matches_expected_output('even-clean', output['even-clean'])
+    output = get_status([repo])
+    assert matches_expected_output('even-clean', output[repo])
