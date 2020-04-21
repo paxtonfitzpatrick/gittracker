0bdfc791afe13875a15c899406da0a6f5072e841
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-04-21 04:56:15 -0400

    fix expected output

--- tests/helpers/functions.py
+++ tests/helpers/functions.py
@@ -99,9 +99,9 @@ def _create_expected_output(repo_name, submodule=False):
         expected['files_staged'] = [(f.change_type, f.a_path, f.b_path)
                                     for f in files_staged]
         expected['n_staged'] = len(files_staged)
-        expected['files_unstaged'] = [(f.change_type, f.a_path, f.b_path)
+        expected['files_not_ staged'] = [(f.change_type, f.a_path, f.b_path)
                                     for f in files_unstaged]
-        expected['n_unstaged'] = len(files_unstaged)
+        expected['n_not_staged'] = len(files_unstaged)
 
         submodules = config.getdict('submodules', 'paths_configs')
         if not any(submodules):
