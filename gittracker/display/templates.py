c2bcb7903f4e71c4757ea67f48b034ad763ee932
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-04-24 13:44:28 -0400

    added info on template usage

--- gittracker/display/templates.py
+++ gittracker/display/templates.py
@@ -22,48 +22,32 @@ ${repos_status}\
 #       3) the active branch is either up to date with its remote
 #          tracking branch or doesn't have one
 #   otherwise, it appears in red
-SINGLE_REPO_V1 = Template("${repo_name}")
+SINGLE_REPO_SIMPLE = Template("${repo_name}")
 
 
-# Single-repository template: verbosity level 2
+# Single-repository template: verbosity levels 2 & 3
 # =======================================================================
-# Default output format; shows more detailed (but not extensive) info:
+# Shows more detailed info; wraps formatting for verbosity levels 2 & 3:
 #   - repo_name: repository name (colored by the same rules as above)
 #   - repo_path: absolute path to the repository
 #   - branch_info: filled by either BRANCH_INFO or BRANCH_INFO_DETACHED,
 #     depending on the state of the branch
-#   - n_uncommitted: the total number of uncommitted changes, including
-#     staged, unstaged, and untracked files
-SINGLE_REPO_V2 = Template(
+#   - local_changes: filled by LOCAL_CHANGES_V2 at verbosity level 2 or
+#     0-3 instances of SINGLE_CHANGE_STATE templates at verbosity level 3.
+#     Can also contain instances of SINGLE_SUBMODULE template (see below)
+SINGLE_REPO_COMPLEX = Template(
 """\
 ${repo_name}
     ${repo_path}
     ${branch_info}
-    ${n_uncommitted} uncommitted changes\
+    ${local_changes}\
 """
 )
 
 
-# Single-repository template: verbosity level 3
+# SUB-TEMPLATES
 # =======================================================================
-# Verbose output format; shows full information given by the `git-status`
-# command for each tracked repository
-#   - repo_name: repository name (colored by the same rules as above)
-#   - repo_path: absolute path to the repository
-#   - branch_info: filled by either BRANCH_INFO or BRANCH_INFO_DETACHED,
-#     depending on the state of the branch
-#   - change_states: filled by up to 3 instances of SINGLE_CHANGE_STATE,
-#   corresponding to staged, unstaged, and untracked files (if any)
-SINGLE_REPO_V3 = Template(
-"""\
-${repo_name}
-    ${repo_path}
-    ${branch_info}
-    ${change_states}
-"""
-)
-
-
+# fills `branch_info` fields for SINGLE_REPO_COMPLEX in normal case
 BRANCH_INFO = Template(
 """\
 on branch: ${local_branch} ${compared_to_remote} ${remote_branch}
@@ -71,15 +55,26 @@ on branch: ${local_branch} ${compared_to_remote} ${remote_branch}
 )
 
 
+# fills `branch_info` fields for SINGLE_REPO_COMPLEX if HEAD is detached
 BRANCH_INFO_DETACHED = Template(
 """\
 ${detached_at} (from branch: ${ref_branch}${ref_sha}) ${new_commits}\
 """
 )
 
-# format skeleton for a single change state (e.g., staged, not staged,
-# untracked) in verbosity level 3. `changed_files` gets filled by instances
-# of `SINGLE_FILE_CHANGE`
+
+# fills `local_changes` field at verbosity level 2:
+#   - n_uncommitted: the total number of uncommitted changes, including
+#     staged, tracked-but-not-staged, and untracked files
+LOCAL_CHANGES_V2 = Template("${n_uncommitted} uncommitted changes")
+
+
+# Describes a single "state" of a locally changed file (staged,
+# tracked-but-not-staged, untracked); one or multiple instances fill
+# `local_changes` field at verbosity level 3 (if any local changes):
+#   - `n_changed`: the number of files in the given state
+#   - `change_state_msg`: a descriptor for the given state
+#   - `changed_files`: filled by instances of `SINGLE_FILE_CHANGE`
 SINGLE_CHANGE_STATE = Template(
 """\
 ${n_changed} ${change_state_msg}:
@@ -87,18 +82,12 @@ ${n_changed} ${change_state_msg}:
 """
 )
 
-# format skeleton for a single modified file in verbosity level 3
+
+# Describes change to a single file; instances fill the `changed_files`
+# field of SINGLE_CHANGE_STATE template at verbosity level 3:
+#   - `change_type`: "modified", "deleted", etc. as shown by Git
 SINGLE_FILE_CHANGE = Template("${change_type}:   ${filepath}")
 
-# replacement skeleton for repositories in detached HEAD states for verbosity
-# levels 2 & 3
-SINGLE_REPO_DETACHED = Template(
-"""\
-${repo_name}
-    ${repo_path}
-    ${detached_head_msg}
-"""
-)
 
 # format skeleton for submodules
 # (analogous to SINGLE_REPO_V2 template; only used with SINGLE_REPO_V3)
@@ -108,12 +97,15 @@ SINGLE_SUBMODULE = Template(
 """
 )
 
+
 # mapping of single repository templates by verbosity value
-REPO_TEMPLATES = {
-    1: SINGLE_REPO_V1,
-    2: SINGLE_REPO_V2,
-    3: SINGLE_REPO_V3
-}
+# REPO_TEMPLATES = {
+#     1: SINGLE_REPO_V1,
+#     2: SINGLE_REPO_V2,
+#     3: SINGLE_REPO_V3
+# }
+
+
 
 # numeric codes from ANSI escape sequences for text color/formatting
 ANSI_SEQS = {
