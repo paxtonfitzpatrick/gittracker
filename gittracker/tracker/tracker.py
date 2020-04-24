d26ad49d0b63b104576274eeb38124503915575e
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-04-24 12:54:06 -0400

    add updated info for detached HEAD cases

--- gittracker/tracker/tracker.py
+++ gittracker/tracker/tracker.py
@@ -67,16 +67,25 @@ def _single_repo_status(repo, verbose, follow_submodules):
     """
     # TODO (future?): option to get info about branches other than current
     status = {
+        # local branch compared to remote tracking branch
         'local_branch': None,
         'remote_branch': None,
         'n_commits_ahead': None,
         'n_commits_behind': None,
+        # uncommitted local changes
         'n_staged': None,
         'files_staged': None,
         'n_not_staged': None,
         'files_not_staged': None,
         'n_untracked': None,
         'files_untracked': None,
+        # alternate info for repos in a detached HEAD state
+        'is_detached': False,
+        'hexsha': None,
+        'ref_branch': None,
+        'ref_sha': None,
+        'detached_commits': None,
+        # info for submodules (if any)
         'submodules': None
     }
     try:
@@ -85,59 +94,68 @@ def _single_repo_status(repo, verbose, follow_submodules):
     except ValueError as e:
         raise InvalidGitRepositoryError(
             "GitTracker currently doesn't support tracking newly "
-            "initialized repositories"
+            f"initialized repositories (can't track {repo.working_dir}"
         ) from e
 
     if repo.head.is_detached:
-        # if HEAD is detached, just report its commit hash...
-        sha_shortened = headcommit.hexsha[:7]
-        return f'HEAD detached at {sha_shortened}'
+        # if HEAD is detached, report some slightly different information
+        status['is_detached'] = True
+        status['hexsha'] = headcommit.hexsha[:7]
+        ref_branch, ref_sha, n_new_commits = _detached_status(repo)
+        status['ref_branch'] = ref_branch
+        if ref_sha != status['hexsha']:
+            # if commits have been made since detaching HEAD, report hash
+            # where initially detached and number of new commits
+            status['ref_sha'] = f'@{ref_sha}'
+            status['detached_commits'] = n_new_commits
 
-    local_branch = repo.active_branch
-    local_branch_name = local_branch.name
-    try:
-        remote_branch_name = local_branch.tracking_branch().name
-        n_ahead = len(list(repo.iter_commits(
-            f"{remote_branch_name}..{local_branch_name}"
-        )))
-        n_behind = len(list(repo.iter_commits(
-            f"{local_branch_name}..{remote_branch_name}"
-        )))
-    except AttributeError:
-        # local branch isn't tracking a remote
-        remote_branch_name = ''
-        n_ahead = None
-        n_behind = None
+    else:
+        local_branch = repo.active_branch
+        local_branch_name = local_branch.name
+        try:
+            remote_branch_name = local_branch.tracking_branch().name
+            n_ahead = len(list(repo.iter_commits(
+                f"{remote_branch_name}..{local_branch_name}"
+            )))
+            n_behind = len(list(repo.iter_commits(
+                f"{local_branch_name}..{remote_branch_name}"
+            )))
+        except AttributeError:
+            # local branch isn't tracking a remote
+            remote_branch_name = ''
+            n_ahead = None
+            n_behind = None
+
+        status['local_branch'] = local_branch_name
+        status['remote_branch'] = remote_branch_name
+        status['n_commits_ahead'] = n_ahead
+        status['n_commits_behind'] = n_behind
 
     staged = headcommit.diff()
     unstaged = repo.index.diff(None)
     untracked = repo.untracked_files
-    status['local_branch'] = local_branch_name
-    status['remote_branch'] = remote_branch_name
-    status['n_commits_ahead'] = n_ahead
-    status['n_commits_behind'] = n_behind
     status['n_staged'] = len(staged)
     status['n_not_staged'] = len(unstaged)
     status['n_untracked'] = len(untracked)
 
-    # computing individual file diff info in all cases would make both
-    # Displayer format methods and unit tests simpler, but skipping it
-    # when unnecessary saves a decent amount of time when many
-    # repositories are tracked
+    # always computing individual file diff info would make Displayer
+    # format methods and unit tests simpler, but skipping when
+    # unnecessary saves noticeable time if dealing with many repositories
     if verbose == 3:
         # go through any staged changes manually to handle renames
         files_staged = []
         for diff in staged:
             a_path = diff.a_path
             change_type = diff.change_type
+            # new filepath only matters if file was renamed
             b_path = diff.b_path if change_type == 'R' else None
             files_staged.append((change_type, a_path, b_path))
 
         status['files_staged'] = files_staged
-        status['files_not_staged'] = list(
-            map(lambda d: (d.change_type, d.a_path), unstaged)
-        )
         status['files_untracked'] = untracked
+        status['files_not_staged'] = [
+            (diff.change_type, diff.a_path) for diff in unstaged
+        ]
 
     if follow_submodules > 0 and any(repo.submodules):
         submodules = {}
@@ -148,6 +166,30 @@ def _single_repo_status(repo, verbose, follow_submodules):
         status['submodules'] = submodules
 
     return status
+
+
+def _detached_status(repo):
+    # TODO: add docstring
+    # log is listed oldest to newest, so reverse it
+    log_entries = repo.head.log()[::-1]
+    for n_new, log_entry in enumerate(log_entries):
+        # log message for checkout takes the format:
+        # "checkout: moving from <old branch> to <new branch/hexsha>"
+        # the most recent checkout is the one that detached HEAD
+        if log_entry.message.startswith('checkout: moving from'):
+            info = log_entry.message.split()
+            ref_branch = info[3]
+            ref_sha = info[-1][:7]
+            break
+
+    else:
+        # fallback/failsafe (shouldn't ever get here):
+        #   - assume HEAD was detached from master and display assumption
+        #   - return the current commit's sha so display excludes other info
+        ref_branch = 'master [assumed]'
+        ref_sha = log_entries[0].newhexsha[:7]
+
+    return ref_branch, ref_sha, n_new
 
 
 def _submodule_status(submodule, depth=1):
