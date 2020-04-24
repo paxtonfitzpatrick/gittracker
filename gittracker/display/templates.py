44a72f9a88ddab1fe86c0309e39e4aa8b9b91b0c
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-04-24 12:54:40 -0400

    new scheme for building display output from templates

--- gittracker/display/templates.py
+++ gittracker/display/templates.py
@@ -1,7 +1,8 @@
 from string import Template
 
 
-# wrapper template for full output. Applies to all verbosity levels
+# Wrapper template for full output. Applies to all verbosity levels
+# =======================================================================
 OUTER_TEMPLATE = Template(
 """\
 ${pkg_ascii_logo}
@@ -11,30 +12,68 @@ ${repos_status}\
 """
 )
 
-# single-repository template: verbosity level 1
+
+# Single-repository template: verbosity level 1
+# =======================================================================
+#   only information shown is the repository's name.  The name is
+#   colored green if:
+#       1) the working tree is clean
+#       2) the active branch is not detached, AND
+#       3) the active branch is either up to date with its remote
+#          tracking branch or doesn't have one
+#   otherwise, it appears in red
 SINGLE_REPO_V1 = Template("${repo_name}")
 
-# single-repository template: verbosity level 2
+
+# Single-repository template: verbosity level 2
+# =======================================================================
+# Default output format; shows more detailed (but not extensive) info:
+#   - repo_name: repository name (colored by the same rules as above)
+#   - repo_path: absolute path to the repository
+#   - branch_info: filled by either BRANCH_INFO or BRANCH_INFO_DETACHED,
+#     depending on the state of the branch
+#   - n_uncommitted: the total number of uncommitted changes, including
+#     staged, unstaged, and untracked files
 SINGLE_REPO_V2 = Template(
 """\
 ${repo_name}
     ${repo_path}
-    on branch ${local_branch} ${compared_to_remote} ${remote_branch}
+    ${branch_info}
     ${n_uncommitted} uncommitted changes\
 """
 )
 
-# single-repository template: verbosity level 3
-# Note: this one has to be constructed somewhat indirectly in order to
-# accommodate different possible states of the repository (e.g., presence or
-# lack of staged/unstaged changes). `file_states` gets filled by instances of
-# `SINGLE_CHANGE_STATE` (if any)
+
+# Single-repository template: verbosity level 3
+# =======================================================================
+# Verbose output format; shows full information given by the `git-status`
+# command for each tracked repository
+#   - repo_name: repository name (colored by the same rules as above)
+#   - repo_path: absolute path to the repository
+#   - branch_info: filled by either BRANCH_INFO or BRANCH_INFO_DETACHED,
+#     depending on the state of the branch
+#   - change_states: filled by up to 3 instances of SINGLE_CHANGE_STATE,
+#   corresponding to staged, unstaged, and untracked files (if any)
 SINGLE_REPO_V3 = Template(
 """\
 ${repo_name}
     ${repo_path}
-    on branch ${local_branch} ${compared_to_remote} ${remote_branch}
+    ${branch_info}
     ${change_states}
+"""
+)
+
+
+BRANCH_INFO = Template(
+"""\
+on branch: ${local_branch} ${compared_to_remote} ${remote_branch}
+"""
+)
+
+
+BRANCH_INFO_DETACHED = Template(
+"""\
+${detached_at} (from branch: ${ref_branch}${ref_sha}) ${new_commits}\
 """
 )
 
