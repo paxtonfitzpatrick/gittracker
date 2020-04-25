b8618c75606ab1e8163faf633af2bc91468f452d
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-04-25 16:18:59 -0400

    finished config validation function

--- tests/helpers/functions.py
+++ tests/helpers/functions.py
@@ -1,7 +1,7 @@
 import pickle
 import shlex
 from collections import namedtuple
-from configparser import ConfigParser
+from configparser import ConfigParser, NoOptionError, NoSectionError
 from pathlib import Path
 from shutil import copy2
 from subprocess import PIPE, run
@@ -14,7 +14,8 @@ from .constants import (
 )
 
 
-def add_submodule_config(filename, dest_dir):
+def add_submodule_config(dest_dir):
+    filename = f"{dest_dir.name}.cfg"
     src = REPO_CONFIGS_DIR.joinpath('submodule-configs', filename)
     assert src.is_file()
     assert dest_dir.is_dir()
@@ -51,7 +52,7 @@ def create_expected_output(config_path, submodule=False):
                                 for f in files_unstaged]
     expected['n_not_staged'] = len(files_unstaged)
 
-    submodules = config.getdict('submodules', 'paths_configs')
+    submodules = config.getdict('submodules', 'paths')
     if not any(submodules):
         expected['submodules'] = None
     else:
@@ -66,6 +67,178 @@ def create_expected_output(config_path, submodule=False):
             pickle.dump(expected, f)
 
     return expected
+
+
+def load_validate_config(config_path):
+    """
+    run some checks for proper config formatting upfront. This helps
+    prevent errors due to config formatting from cropping up during
+    actual tests, where they can be hard to differentiate from actual
+    test failures
+    """
+    config_file = config_path.name
+    config = ConfigParser(converters=CONVERTERS)
+    with open(config_path, 'r') as f:
+        config.read_file(f)
+
+    # validate section and option presence and names
+    # ===================================================================
+    sections_options = {
+        'repo': ('untracked_files', 'staged_changes', 'unstaged_changes'),
+        'active_branch': ('name', 'remote_branch', 'n_commits_ahead',
+                          'n_commits_behind'),
+        'head': ('is_empty', 'is_detached', 'hexsha', 'from_branch', 'ref_sha',
+                 'detached_commits'),
+        'submodules': ('paths',)
+    }
+    required_sections = sections_options.keys()
+    present_sections = config.sections()
+    bad_sections = [s for s in present_sections if s not in required_sections]
+    if any(bad_sections):
+        message = "config must contain only the following sections: " \
+                  f"{', '.join(required_sections)}\n(contains: " \
+                  f"{', '.join(bad_sections)}) "
+        raise InvalidConfigValue(config_file, message=message)
+
+    for section, options in sections_options.items():
+        try:
+            for option in options:
+                config.get(section, option)
+
+            bad_options = [opt for opt in config[section] if opt not in options]
+            assert len(bad_options) == 0
+
+        except Exception as e:
+            if isinstance(e, NoSectionError):
+                message = f"missing required section: {section}"
+                s = None
+            elif isinstance(e, NoOptionError):
+                message = f"missing required option: {option}"
+                s = section
+            elif isinstance(e, AssertionError):
+                message = f"contains unrecognized option: {option}"
+                s = section
+            else:
+                message = "unexpected exception in parsing options/values"
+                s = None
+            raise InvalidConfigValue(config_file, s, message)
+
+    # validate staged_changes and unstaged_changes value formatting
+    # ===================================================================
+    for option in ('staged_changes', 'unstaged_changes'):
+        valid_change_types = ('R', 'M', 'A', 'D')
+        if option == 'unstaged_changes':
+            valid_change_types = valid_change_types[1:]
+        lines = config.get('repo', option).strip.splitlines()
+        for val in lines:
+            if val.count(':') != 2:
+                message = f"{option} value should be a newline-separated " \
+                          "list, where each line contains three double colon " \
+                          "(::)-separated values: the change type, a_path, " \
+                          "and b_path"
+                raise InvalidConfigValue(config_file, 'repo', message)
+
+            change_type = val.split('::')[0].strip()
+            if change_type not in valid_change_types:
+                message = f"invalid change type in {option} value " \
+                          f"({change_type}). Valid change types are: " \
+                          f"{', '.join(valid_change_types)}"
+                raise InvalidConfigValue(config_file, 'repo', message)
+
+    # validate mutual exclusivity of is_empty and is_detached
+    # ===================================================================
+    try:
+        is_empty = config.getboolean('head', 'is_empty')
+        is_detached = config.getboolean('head', 'is_detached')
+        assert not (is_empty and is_detached)
+    except Exception as e:
+        if isinstance(e, ValueError):
+            message = "both is_empty and is_detached fields must be booleans"
+        elif isinstance(e, AssertionError):
+            message = "is_empty and is_detached may not both be true"
+        else:
+            message = "unexpected exception in is_empty/is_detached values"
+        raise InvalidConfigValue(config_file, 'head', message) from e
+
+    # validate options required when is_empty is/isn't set to true
+    # ===================================================================
+    if is_detached:
+        for val in ('hexsha', 'from_branch', 'ref_sha', 'detached_commits'):
+            if config.get('head', 'val') == '':
+                message = f"{val} is required if is_detached is set to true"
+                raise InvalidConfigValue(config_file, 'head', message)
+
+        for val in ('hexsha', 'ref_sha'):
+            if len(config.get('head', val)) < 7:
+                message = f"{val} must be 7 characters (code expects 40 characters)"
+                raise InvalidConfigValue(config_file, 'head', message)
+
+        try:
+            detached_commits = config.getint('head', 'detached_commits')
+        except ValueError as e:
+            message = "detached_commits must be an integer"
+            raise InvalidConfigValue(config_file, 'head', message) from e
+
+        hexsha_short = config.get('head', 'hexsha')[:7]
+        ref_sha_short = config.get('head', 'ref_sha')[:7]
+
+        if (detached_commits == 0) is not (hexsha_short == ref_sha_short):
+            message = "if detached_commits is set to 0, hexsha and ref_sha " \
+                      "must match. Otherwise, they must be different"
+            raise InvalidConfigValue(config_file, 'head', message)
+
+    elif not is_empty and config.get('active_branch', 'remote_branch') != '':
+        # active branch values matter only if is_empty and is_detached are false
+        if '/' not in config.get('active_branch', 'remote_branch'):
+            message = "remote_branch format should be: <remote_name>/<branch_name>"
+            raise InvalidConfigValue(config_file, 'active_branch', message)
+
+        for option in ('n_commits_ahead', 'n_commits_behind'):
+            try:
+                config.getint('active_branch', option)
+            except ValueError as e:
+                message = f"{option} must be an integer if remote_branch is set"
+                raise InvalidConfigValue(config_file, 'active_branch', message)
+
+    # validate submodule path formatting and existence of config files
+    # ===================================================================
+    sm_paths = config.get('submodules', 'paths').strip().splitlines()
+    # will be skipped if no submodules are set
+    for sm_path in sm_paths:
+        if sm_path.startswith('/'):
+            message = "submodule paths should be relative (from the parent " \
+                      "repository's root directory), not absolute"
+            raise InvalidConfigValue(config_file, 'submodules', message)
+        elif '..' in sm_path:
+            message = "submodule paths cannot be outside the parent " \
+                      "repository (cannot include '..')"
+            raise InvalidConfigValue(config_file, 'submodules', message)
+
+        sm_config_name = f'{sm_path.name}.cfg'
+        sm_config_path = REPO_CONFIGS_DIR.joinpath('submodule-configs',
+                                                   sm_config_name)
+        if not sm_config_path.is_file():
+            message = f"missing config file for submodule: {sm_path}.\n" \
+                      f"Expected file at {sm_config_path}"
+            raise InvalidConfigValue(config_file, 'submodules', message)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def matches_expected_output(repo_name, test_output, verbose=2, include_submodules=False):
@@ -100,22 +273,12 @@ def run_command(cmd):
 #########################################################################
 #                Custom type converters for ConfigParser                #
 #########################################################################
-def _get_multiline_list(val):
+def _get_path_list(val):
     """
     splits a newline-separated list of values for a single option into a
     list
     """
     return val.strip().splitlines()
-
-
-def _get_dict(val):
-    """
-    splits a newline separated list of values for a single option (where
-    each option takes the format `sub-option = sub-value`) into a
-    dictionary with the format `{sub-option: sub-value}`
-    """
-    keys_vals = map(lambda x: x.split('::'), val.strip().splitlines())
-    return {k.strip(): v.strip() for k, v in keys_vals}
 
 
 def _get_diff_list(val):
@@ -135,7 +298,6 @@ def _get_diff_list(val):
 
 CONVERTERS = {
     'difflist': _get_diff_list,
-    'dict': _get_dict,
-    'list': _get_multiline_list
+    'pathlist': _get_path_list
 }
 
